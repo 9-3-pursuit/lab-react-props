f7cadd834d0743fa0c6398a794cfd5ae5b0825e9
Marangely Rosas <Ladykaliber22@gmail.com>
2023-02-01 15:24:57 -0500

    Marangely's Completed Lab-React-Props

--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,3 +1,15 @@
-export default function RecentDonations() {
-  return null;
+export default function RecentDonations(props) {
+  console.log(props.recentDonations)
+  return (
+  <section>
+  <h2>Recent Donations</h2>
+  <ul>
+  {props.recentDonations.map((donated)=> {
+    return (
+      <li key={donated.id}><span>{donated.name} donated ${donated.amount}</span>{donated.caption}</li>
+    )
+  })}
+  </ul>
+</section>
+  )
 }
