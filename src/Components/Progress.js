3c5bf651b451d77a93cccf03a34cd38175190638
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-02-01 13:44:07 -0500

    Add props to Progress

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,3 +1,11 @@
-export default function Progress() {
-  return null;
+const Progress = (props) => {
+  return(
+    <section className="progress">
+    <h2>
+      Raised <span className="secondary">$0</span> of
+      <span className="secondary">$1000</span>
+    </h2>
+    </section>
+  )
 }
+export default  Progress
