eb55b1f57271af5fb6575a4914bda59c613baec0
Keeanu Rodriguez <keeanu7@gmail.com>
2023-02-01 14:02:49 -0500

    all test done and css done

--- src/Components/DonationForm.js
+++ src/Components/DonationForm.js
@@ -1,3 +1,24 @@
-export default function DonationForm() {
-  return null;
+import "./DonationFrom.css"
+
+export default function DonationForm(props) {
+  return (
+    <div>
+      <form>
+        <h3>You could be donation <span>#{props.donations.length++}!</span></h3>
+        <label>Name</label>
+        <br></br>
+        <input type="text" placeholder="Your name..."></input>
+        <br></br>
+        <label>Caption</label>
+        <br></br>
+        <input type="text" placeholder="Add a brief message..."></input>
+        <br></br>
+        <label>Amount</label>
+        <br></br>
+        <input type="text" placeholder="0"></input>
+        <br></br>
+        <button type="Submit">Donate!</button>
+      </form>
+    </div>
+  )
 }
--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,3 +1,13 @@
-export default function Progress() {
-  return null;
+import "./Progress.css"
+
+export default function Progress(props) {
+  let sum = 0;
+  props.donations.map((donation) => {
+    return sum += donation.amount;
+  });
+  return (
+  <div class="progress">
+    <h2> Raised <span>${sum}</span> of <span>${props.target}</span></h2>
+  </div>
+  )
 }
