82bcb6f6891a93a101d1a8e98c834fce58db7fee
SangUn Park <Sangunpark1@gmail.com>
2023-02-01 13:12:29 -0500

    React Props Lab 9.3

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,3 +1,13 @@
-export default function Progress() {
-  return null;
+export default function Progress({donations}) {
+  let sum = 0;
+  for (let i = 0; i < donations.length; i++){
+    sum += donations[i].amount
+  }
+  return (
+    <section className="progress">
+    <h2>
+      Raised <span className="secondary">${sum}</span> of <span className="secondary">$1000</span>
+    </h2>
+    </section>
+  )
 }
