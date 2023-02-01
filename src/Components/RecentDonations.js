2ae3997363feed4411eb12c7d6e33c45cb5d6da8
shaquala <96317236+shaqfred@users.noreply.github.com>
2023-02-01 14:37:50 -0500

    added data to App, RecentDonations.js

--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,3 +1,14 @@
-export default function RecentDonations() {
-  return null;
-}
+const RecentDonations = (props) => {
+  const {name, amount, caption} =props
+  return (
+    <section>
+      <h2>RecentDonations</h2>
+      <ul>
+        <li><span>{name} donated ${amount}</span></li>
+      </ul>
+    </section>
+  ) 
+};
+
+export default RecentDonations
+
