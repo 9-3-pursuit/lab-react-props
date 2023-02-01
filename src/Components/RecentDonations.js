0a34c8542cf9c49a66266defe04b487ada380771
riyaalexander <riyaalexander@Riyas-Air.fios-router.home>
2023-02-01 12:25:39 -0500

    React Props

--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,3 +1,18 @@
-export default function RecentDonations() {
-  return null;
-}
+const RecentDonations = (props) => {
+    return(
+      <section>
+  <h2>Recent Donations</h2>
+  <ul>
+    {props.donationGiven.map((donated) =>{
+      return (
+        <li key={donated.id}><span>{donated.name} donated ${donated.amount}</span>{donated.caption}</li>
+      )
+    })}
+    {/* <li><span>Jo donated $25</span>You really need this. Really.</li>
+    <li><span>Rami donated $10</span>Here, take a break from work!</li> */}
+  </ul>
+</section>
+    )
+  };
+
+export default RecentDonations;
