2c07208b067b14f4cfeb7f3e0fbbea73f38d166d
Isaiah Desrosiers <40811090+IDGitHubMan@users.noreply.github.com>
2023-02-01 13:07:38 -0500

    Add recent donations component.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import TopBar from "./Components/TopBar";
 import "./App.css";
 import DonationForm from "./Components/DonationForm";
 import Progress from "./Components/Progress";
+import RecentDonations from "./Components/RecentDonations";
 
 const targetAmount = 1000;
 const donations = [
@@ -49,7 +50,9 @@ function App() {
     <>
       <TopBar />
       <main className="container">
-        <section className="sidebar">{/* Recent Donations */}</section>
+        <section className="sidebar">
+          <RecentDonations recentDonations={donations} />
+        </section>
         <section className="">
           <Progress amount={total} targetAmount={targetAmount} />
           <DonationForm donorPosition={donations.length + 1} />
--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,3 +1,19 @@
-export default function RecentDonations() {
-  return null;
+export default function RecentDonations(props) {
+  return (
+    <section>
+      <h2>Recent Donations</h2>
+      <ul>
+        {props.recentDonations.map((item) => {
+          return (
+            <li key={item.id}>
+              <span>
+                {item.name} donated ${item.amount}
+              </span>
+              {item.caption}
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
 }
