0904822b884b1991d472c618ccb19f2924d65fca
elizabethbright721 <elizabethbright721@gmail.com>
2023-02-01 16:23:50 -0500

    React Props Lab

--- src/Components/DonationForm.js
+++ src/Components/DonationForm.js
@@ -1,3 +1,19 @@
-export default function DonationForm() {
-  return null;
+export default function DonationForm( {donations} ) {
+  return (
+    <div>
+      <h2>You could be donation <span className="secondary">#{donations.length + 1}!</span></h2>
+      <form className="donation">
+        <label>Name <br></br>
+        <input type="text"/>
+        </label><br></br>
+        <label>Caption <br></br>
+        <input type="text" />
+        </label><br></br>
+        <label>Amount <br></br>
+        <input type="text" />
+        </label><br></br>
+        <input type="submit" value="Donate!" />
+      </form>
+    </div>
+  )
 }
--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,3 +1,16 @@
-export default function RecentDonations() {
-  return null;
+export default function RecentDonations( { donations } ) {
+  const recentDonation = donations.map((donation) => {
+    return <li><span className="sidebar">{donation.name} donated ${donation.amount}</span> 
+    {donation.caption}</li>
+        
+      
+  })
+
+  
+  return (
+    <div>
+      <h1>Recent Donations</h1>
+      <ul>{ recentDonation }</ul>
+    </div>
+  );
 }
