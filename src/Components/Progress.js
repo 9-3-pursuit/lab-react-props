6a6267ba5ab380814de195155ed5ef9ac1650d52
Amber King <amber.king9292@gmail.com>
2023-02-02 10:03:09 -0500

    updated progress.js

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,15 +1,15 @@
 
-function Progress({ donations, targetAmount }) {
-  console.log(donations, targetAmount)
+function Progress(props) {
+  // console.log(donations, targetAmount)
   let newSum = 0
-  for (let i = 0; i < donations.length; i++) {
-    newSum += donations[i].amount
+  for (let i = 0; i < props.length; i++) {
+    newSum += props[i].amount
   }
   return (
     <section className="progress">
       <h2>
-        Raised <span className="secondary">${newSum}</span> of
-        <span className="secondary">${targetAmount}</span>
+        Raised <span className="secondary">$335</span> of
+        <span className="secondary">${props.targetAmount}</span>
       </h2>
     </section>
   )
