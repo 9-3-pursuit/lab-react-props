05530ccceea1f89d16362e7ac9b0fa0c36b814ca
ariunaamy <ariun.myagmar@gmail.com>
2023-02-01 22:19:23 -0500

    complete

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,3 +1,17 @@
-export default function Progress() {
-  return null;
+function Progress(props) {
+  let raised = 0;
+  console.log(props.targetAmount)
+  for (let i=0; i<props.donations.length; i++){
+   raised += Number(props.donations[i].amount)
+  }
+ return (
+  <section className="progress">
+    <h2>
+      Raised <span className="secondary">${raised}
+      </span> of <span className="secondary">${props.targetAmount}</span>
+    </h2>
+  </section>
+ )
 }
+
+export default Progress
--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,3 +1,13 @@
-export default function RecentDonations() {
-  return null;
+function RecentDonations(props) {
+  return (
+    <section>
+      <h2>Recent Donations</h2>
+      <ul>
+        {props.donations.map((donation, i)=>{
+         return <li key={i}><span>{donation.name} donated ${donation.amount}</span> {donation.caption}</li>})}
+      </ul>
+    </section>
+  )
 }
+
+export default RecentDonations
