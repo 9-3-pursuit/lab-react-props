6ea56846102a71b70196905c7e4f56b997b85289
Fernando Cortez <cortez.fernando11216@gmail.com>
2023-02-02 09:33:32 -0500

    imported recent donations, progress and donationform

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,3 +1,20 @@
-export default function Progress() {
-  return null;
+
+function Progress({donations, targetAmount}) {
+console.log(donations, targetAmount)
+
+let sum = 0; 
+for (let i = 0; i < donations.length; i++) {
+ sum += Number(donations[i].amount) //loop to get the amount from the array
 }
+return(
+  <section className="progress">
+  <h2>
+    Raised <span className="secondary">${sum}</span> of 
+    <span className="secondary">  ${targetAmount}</span>
+  </h2>
+</section>
+
+)
+
+}
+export default Progress
