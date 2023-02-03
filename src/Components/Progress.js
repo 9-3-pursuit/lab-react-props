4e02460a1a2b13f156006336578e002ca4251360
Karimah Reavis <Karimah586@gmail.com>
2023-02-03 10:00:18 -0500

    lab

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -1,3 +1,15 @@
-export default function Progress() {
-  return null;
+export default function Progress({allDonations, target}) {
+console.log("all donations", allDonations);
+let donated = 0;
+for(let donate of allDonations.amount){
+  donated += donate
+}
+
+
+  return <section className="progress">
+  <h2>
+    Raised <span className="secondary">$0</span> of
+    <span className="secondary">${target}</span>
+  </h2>
+</section>
 }
