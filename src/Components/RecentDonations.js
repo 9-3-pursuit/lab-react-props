1ba50744a15261a6b0931af7dbb969abacab24cd
Taylor Brooks <tybrooks01@icloud.com>
2023-02-03 13:09:09 -0500

    react props lab

--- src/Components/RecentDonations.js
+++ src/Components/RecentDonations.js
@@ -1,3 +1,16 @@
-export default function RecentDonations() {
-  return null;
+export default function RecentDonations(props) {
+  return (
+<section>
+  <h2>Recent Donations</h2>
+  <ul>
+    {props.donationGiven.map((donated) => {
+      return(
+        <li key={donated.id}><span>{donated.name} donated ${donated.amount}</span>{donated.caption}</li>
+      )
+    })
+    }
+  </ul>
+</section>
+  )
+  
 }
